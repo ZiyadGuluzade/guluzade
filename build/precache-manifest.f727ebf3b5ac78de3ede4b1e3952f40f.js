45df31eeb2342831230481180f2556d148484d19
Z.Guluzade <ziyad_guluzade@outlook.com>
2020-05-07 21:10:30 -0400

    npm run build

---
+++ build/precache-manifest.f727ebf3b5ac78de3ede4b1e3952f40f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5e6ef3c053e8d64728b",
+    "url": "/static/css/main.dff0964b.chunk.css"
+  },
+  {
+    "revision": "b5e6ef3c053e8d64728b",
+    "url": "/static/js/main.5f2f50e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b1aafb37b0667db7364",
+    "url": "/static/js/2.367fa8ef.chunk.js"
+  },
+  {
+    "revision": "d5dfac86ba7ab66d01b3a4a6c2b0e650",
+    "url": "/static/media/resume.d5dfac86.pdf"
+  },
+  {
+    "revision": "1c619d3c0f617916d4c83206a9254098",
+    "url": "/static/media/black_geo_mobile.1c619d3c.jpg"
+  },
+  {
+    "revision": "6f033c441685157b75bcaf270316e01a",
+    "url": "/static/media/black_geo_tablet.6f033c44.jpg"
+  },
+  {
+    "revision": "9d413924fd6ee9bd1525eae82bd04ecc",
+    "url": "/static/media/black_geo.9d413924.jpg"
+  },
+  {
+    "revision": "d1e0d8cd4c745f4b6484b2a7e6dc8919",
+    "url": "/static/media/crystall_collector.d1e0d8cd.jpg"
+  },
+  {
+    "revision": "c0d899ae192eace84ce48d1b59cc41ca",
+    "url": "/static/media/rock_paper_scissors.c0d899ae.png"
+  },
+  {
+    "revision": "2f630c8ff0e603d2b159c169d368b7d0",
+    "url": "/static/media/atp_tour_logo.2f630c8f.jpg"
+  },
+  {
+    "revision": "74da7e5bf669d90bff7dd3e5306f7bd3",
+    "url": "/static/media/google_books_app.74da7e5b.jpg"
+  },
+  {
+    "revision": "f6c3ac6bb1a4374cfce50f01bb1f58ae",
+    "url": "/static/media/jeopardy-game.f6c3ac6b.jpeg"
+  },
+  {
+    "revision": "b403094e31d965fd827c033cfe37a57a",
+    "url": "/static/media/password_generator.b403094e.jpg"
+  },
+  {
+    "revision": "bddcd349b92e24bd39ed8a0e987edba7",
+    "url": "/index.html"
+  }
+];
